56b39f7a24eabb0b35af04e9215381d63af1bae1
Adhishri123 <deshmukhadhishri123@gmail.com>
2026-04-05 15:00:02 +0530

    register page ui

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Register.css";
 
 function Register() {
     return (
-        <h1>Registration</h1>
+        <div className="register-container">
+            <div className="register-box">
+                <h2>Registration</h2>
+
+                <form>
+                    <div className="input-fields">
+                        <label>Full Name</label>
+                        <input type="text" placeholder="Enter your full name" required></input>
+                        <small className="error">{/* Error placeholder */}</small>
+                    </div>
+                    <div className="input-fields">
+                        <label>Email</label>
+                        <input type="email" placeholder="Enter your email ID" required></input>
+                        <small className="error"></small>
+                    </div>
+                    <div className="input-fields">
+                        <label>Password</label>
+                        <input type="password" placeholder="Enter password" required></input>
+                        <small className="error"></small>
+                    </div>
+                    <div className="input-fields">
+                        <label>Confirm Password</label>
+                        <input type="password" placeholder="Confirm password" required></input>
+                        <small className="error"></small>
+                    </div>
+
+                    <button type="submit" className="register-btn">
+                        Register
+                    </button>
+
+                    <p className="login-page-link">
+                        Already have an account?{" "}<Link to="/">Login</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
     );
 }
 
